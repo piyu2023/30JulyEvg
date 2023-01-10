488b332188bf4c3296283667a7864776564f90eb
hp <hp@LAPTOP-L00DVKK3>
2023-01-10 18:18:13 +0530

    [Priyanka]:New code

---
+++ src/Repository/SampleGit.java
@@ -0,0 +1,11 @@
+package Repository;
+
+public class SampleGit 
+{
+public static void main(String[] args) 
+{
+	
+System.out.println("Hii team velocity");	
+System.out.println("Pushing code in Github");
+}
+}
